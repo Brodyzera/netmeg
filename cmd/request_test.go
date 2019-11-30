90f3e9d5e2ee65cdc8e2ece7bf1b09e2da082f5d
Brody Smith <brodygs9630@gmail.com>
2019-11-30 16:23:08 -0700

    add unit tests for prepareHeaders and writeResultsToConsole

--- cmd/request_test.go
+++ cmd/request_test.go
@@ -25,3 +25,35 @@ func TestWriteResultsToFile(t *testing.T) {
 		t.Errorf("TestWriteResultsToFile failed, error occured: %s\n", err)
 	}
 }
+
+func TestWriteResultsToConsole(t *testing.T) {
+	requestResults := []requestResult{
+		requestResult{
+			statusCode: 200,
+			body:       "This is a test response body",
+		},
+		requestResult{
+			statusCode: 200,
+			body:       "This is another test response body",
+		},
+	}
+
+	err := writeResultsToConsole(requestResults)
+	if err != nil {
+		t.Errorf("TestWriteResultsToConsole failed, error occured: %s\n", err)
+	}
+}
+
+func TestPrepareHeaders(t *testing.T) {
+	headersCorrect := "Content-Type:    application/json,     Test:another/header123"
+	headersIncorrect := "Content-Type - application/json, Test:another/header123"
+	_, errorCorrect := prepareHeaders(headersCorrect)
+	if errorCorrect != nil {
+		t.Errorf("TestPrepareHeaders failed, error occured: %s\n", errorCorrect)
+	}
+
+	_, errorIncorrect := prepareHeaders(headersIncorrect)
+	if errorIncorrect == nil {
+		t.Errorf("TestPrepareHeaders failed, expected header error didn't occur!\n")
+	}
+}
